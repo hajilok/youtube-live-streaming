91431691147af3ebb271f3e42416b1838536bee6
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2023-10-28 00:39:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({ input, output });
 
 const streamkey = await rl.question('Send Your Stream Key On Youtube ? ( This Is Screet key) ');
 const video = await rl.question('Send Your filename video ?  ? ( Example : video.mp4 ) ');
-const audio = await rl.question('Send Your filename audio ? ( Example : audio.mp3  ) ');
+const audio = "https://stream.zeno.fm/0r0xa792kwzuv";
 
 
 const command = `ffmpeg -stream_loop -1 -re -i ${video} -stream_loop -1 -re -i ${audio} -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 1500k -f flv  rtmp://a.rtmp.youtube.com/live2/${streamkey}
