67e99b89a63a4883039f8cfa292b7128af80ba18
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2023-10-28 19:00:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const video = await rl.question('Send Your filename video ?  ? ( Example : video
 const audio = "https://stream.zeno.fm/0r0xa792kwzuv";
 
 
-const command = `ffmpeg -stream_loop -1 -re -i ${video} -stream_loop -1 -re -i ${audio} -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 1500k -f flv  rtmp://a.rtmp.youtube.com/live2/${streamkey}
-`;
-
-const command = `ffmpeg -stream_loop -1 -re -i ${video} -stream_loop -1 -re -i ${audio} -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 1500k -f flv  rtmp://a.rtmp.youtube.com/live2/${streamkey}
-`;
+const command = [
+  '-stream_loop', '-1', '-re', '-i', video,
+  '-stream_loop', '-1', '-re', '-i', audio,
+  '-vcodec', 'libx264', '-pix_fmt', 'yuvj420p', '-maxrate', '2048k',
+  '-preset', 'ultrafast', '-r', '12', '-framerate', '1', '-g', '50',
+  '-crf', '51', '-c:a', 'aac', '-b:a', '128k', '-ar', '44100',
+  '-strict', 'experimental', '-video_track_timescale', '100',
+  '-b:v', '1500k', '-f', 'flv',
+  `rtmp://a.rtmp.youtube.com/live2/${streamkey}`,
+];
 
 const child = spawn('ffmpeg' , command)
 
