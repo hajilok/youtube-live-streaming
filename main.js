e9527940e20349d4bb5a0676f34b9e1a3b468614
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2023-10-28 18:54:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { exec } from 'child_process'
+import { spawn } from 'child_process'
 import 'dotenv/config';
 const server = express()
 
@@ -11,13 +11,18 @@ const audio = "https://stream.zeno.fm/0r0xa792kwzuv";
 const command = `ffmpeg -stream_loop -1 -re -i ${video} -stream_loop -1 -re -i ${audio} -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 1500k -f flv  rtmp://a.rtmp.youtube.com/live2/${streamkey}
 `;
 
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
+const child = spawn('ffmpeg' , command)
+
+child.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+child.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
 });
 
 server.use('/', (req, res) => {
