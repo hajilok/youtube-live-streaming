2e5c3ddcfc84865251b95bebd1dc0baef30c6503
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2023-10-29 09:01:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { exec } from 'child_process'
+import { spawn } from 'child_process'
 import 'dotenv/config';
 const server = express()
 
@@ -7,18 +7,50 @@ const streamkey = process.env.streamkey
 const video = "hajilok.mov"
 const audio = "https://stream.zeno.fm/0r0xa792kwzuv";
 
+const ffmpegCommand = [
+  'ffmpeg',
+  '-stream_loop', '-1',
+  '-re',
+  '-i', video,
+  '-stream_loop', '-1',
+  '-re',
+  '-i', audio,
+  '-vcodec', 'libx264',
+  '-pix_fmt', 'yuvj420p',
+  '-maxrate', '2048k',
+  '-preset', 'ultrafast',
+  '-r', '12',
+  '-framerate', '1',
+  '-g', '50',
+  '-crf', '51',
+  '-c:a', 'aac',
+  '-b:a', '128k',
+  '-ar', '44100',
+  '-strict', 'experimental',
+  '-video_track_timescale', '100',
+  '-b:v', '1500k',
+  '-f', 'flv',
+  `rtmp://a.rtmp.youtube.com/live2/${streamkey}`,
+];
 
-const command = `ffmpeg -stream_loop -1 -re -i ${video} -correct_ts_overflow -stream_loop -1 -re -i ${audio} -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 1500k -f flv  rtmp://a.rtmp.youtube.com/live2/${streamkey}
-`;
+const child = spawn(ffmpegCommand[0], ffmpegCommand.slice(1));
 
-exec(command, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
+child.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
 });
+
+child.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+child.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+child.on('error', (err) => {
+  console.error(`Child process error: ${err}`);
+});
+
 server.use('/', (req, res) => {
   res.send('Your Live Streaming Is All Ready Live')
 })
