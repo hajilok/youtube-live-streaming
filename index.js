cce895d825f3a0ef5b928fe7aad14246782b86ab
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2023-10-29 09:48:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,48 @@ const streamkey = await rl.question('Send Your Stream Key On Youtube ? ( This Is
 const video = await rl.question('Send Your filename video ?  ? ( Example : video.mp4 ) ');
 const audio = "https://stream.zeno.fm/0r0xa792kwzuv";
 
+const ffmpegCommand = [
+  'ffmpeg',
+  '-stream_loop', '-1',
+  '-re',
+  '-i', video,
+  '-stream_loop', '-1',
+  '-re',
+  '-i', audio,
+  '-vcodec', 'libx264',
+  '-pix_fmt', 'yuvj420p',
+  '-maxrate', '2048k',
+  '-preset', 'ultrafast',
+  '-r', '12',
+  '-framerate', '1',
+  '-g', '50',
+  '-crf', '51',
+  '-c:a', 'aac',
+  '-b:a', '128k',
+  '-ar', '44100',
+  '-strict', 'experimental',
+  '-video_track_timescale', '100',
+  '-b:v', '1500k',
+  '-f', 'flv',
+  `rtmp://a.rtmp.youtube.com/live2/${streamkey}`,
+];
 
-const command = `ffmpeg -stream_loop -1 -re -i ${video} -correct_ts_overflow -stream_loop -1 -re -i ${audio} -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 1500k -f flv  rtmp://a.rtmp.youtube.com/live2/${streamkey}
-`;
+const child = spawn(ffmpegCommand[0], ffmpegCommand.slice(1));
 
-exec(command, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
+child.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
 });
+
+child.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
 child.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
+});
+
+child.on('error', (err) => {
+  console.error(`Child process error: ${err}`);
 });
 
 server.use('/', (req, res) => {
